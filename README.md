38796a9882e27c2e4248aedd38b90ca136e17ad1
Emil Hammarstrom <emil.a.hammarstrom@gmail.com>
2021-06-06 15:34:57 +0200

    Working implementation with benchmarks
    Signed-off-by: Emil Hammarstrom <emil.a.hammarstrom@gmail.com>

--- README.md
+++ README.md
@@ -1,7 +1,58 @@
-# Intelhexes
+# intelhexes
 
 Parses intelhex
 
-## Format description
+## format description
+
+https://en.wikipedia.org/wiki/Intel_HEX
 
 https://www.keil.com/support/docs/1584/
+
+## intelhexes vs python-intelhex
+
+### Performance
+
+Measured using hyperfine on a laptop with 16GB RAM and an i7-9750H,
+
+`hyperfine -w10 -m100 PROGRAM`
+
+|                   | intelhexes                                       | python-intelhex                                    |
+| ----------------- | ------------------------------------------------ | -------------------------------------------------- |
+| Time (mean ± σ)   | `3.3 ms ± 1.1 ms [User: 2.7 ms, System: 0.7 ms]` | `40.1 ms ± 0.6 ms [User: 37.0 ms, System: 3.2 ms]` |
+| Range (min … max) | `2.3 ms … 8.7 ms 962 runs`                       | `39.0 ms … 42.0 ms 100 runs`                       |
+
+### Output
+
+Differences: python-intelhex re-aligns addresses if a sub-16-byte data address is encountered
+
+Snippets,
+
+intelhexes:
+
+```
+0x008260  FC 8F FF FF 43 61 6E 27  74 20 69 6E 69 74 69 61 |....Can't initia|
+0x008270  6C 69 7A 65 20 6D 75 74  65 78 2C 20 77 61 73 20 |lize mutex, was |
+0x008280  4E 55 4C 4C 0D 0A 00 00  43 61 6E 27 74 20 75 6E |NULL....Can't un|
+0x008290  6C 6F 63 6B 20 6D 75 74  65 78 2C 20 77 61 73 20 |lock mutex, was |
+0x0082a0  4E 55 4C 4C 0D 0A 00 00  43 6F 75 6C 64 20 6E 6F |NULL....Could no|
+0x0082b0  74 20 6C 6F 63 6B 20 70  6F 77 65 72 20 73 61 76 |t lock power sav|
+0x0082c0  65 20 6D 75 74 65 78 00                          |e mutex.        |
+0x0082c8  04 00 02 00 00 14 00 00  00 00 00 00 02 00 02 00 |................|
+0x0082d8  00 0E 5C 04 05 06 07 08  01 11 00 00 25 26 27 03 |..\.........%&'.|
+0x0082e8  3F 49 F6 D4 A3 C5 5F 38  74 C9 B3 E3 D2 10 3F 50 |?I...._8t.....?P|
+```
+
+python-intelhex:
+
+```
+8260  FC 8F FF FF 43 61 6E 27 74 20 69 6E 69 74 69 61  |....Can't initia|
+8270  6C 69 7A 65 20 6D 75 74 65 78 2C 20 77 61 73 20  |lize mutex, was |
+8280  4E 55 4C 4C 0D 0A 00 00 43 61 6E 27 74 20 75 6E  |NULL....Can't un|
+8290  6C 6F 63 6B 20 6D 75 74 65 78 2C 20 77 61 73 20  |lock mutex, was |
+82A0  4E 55 4C 4C 0D 0A 00 00 43 6F 75 6C 64 20 6E 6F  |NULL....Could no|
+82B0  74 20 6C 6F 63 6B 20 70 6F 77 65 72 20 73 61 76  |t lock power sav|
+82C0  65 20 6D 75 74 65 78 00 04 00 02 00 00 14 00 00  |e mutex.........|
+82D0  00 00 00 00 02 00 02 00 00 0E 5C 04 05 06 07 08  |..........\.....|
+82E0  01 11 00 00 25 26 27 03 3F 49 F6 D4 A3 C5 5F 38  |....%&'.?I...._8|
+82F0  74 C9 B3 E3 D2 10 3F 50 4A FF 60 7B EB 40 B7 99  |t.....?PJ.`{.@..|
+```
--- src/ringbuffer.rs
+++ src/ringbuffer.rs
@@ -10,6 +10,7 @@ use std::mem;
 #[derive(Debug)]
 pub enum RBErrorKind {
     Empty,
+    PeekReachedWrap,
     NotEnoughBytesAvail,
 }
 
@@ -18,6 +19,7 @@ pub type RBResult<T> = std::result::Result<T, RBErrorKind>;
 #[derive(Debug)]
 pub struct RingBuffer<const SIZE: usize> {
     buf: [u8; SIZE],
+    peek_buffer: [u8; SIZE],
     read_idx: usize,
     bytes_avail: usize,
 }
@@ -26,6 +28,7 @@ impl<const SIZE: usize> RingBuffer<SIZE> {
     pub fn new() -> RingBuffer<SIZE> {
         RingBuffer {
             buf: [0u8; SIZE],
+            peek_buffer: [0u8; SIZE],
             read_idx: 0,
             bytes_avail: 0,
         }
@@ -53,12 +56,15 @@ impl<const SIZE: usize> RingBuffer<SIZE> {
             let can_read = SIZE - post_write_idx;
             written += buf.read(&mut self.buf[post_write_idx..post_write_idx + can_read])?;
             self.bytes_avail += written;
+            assert!(self.bytes_avail <= SIZE);
         }
 
         let write_idx = (self.read_idx + self.bytes_avail) % SIZE;
         if write_idx < self.read_idx {
-            written += buf.read(&mut self.buf[write_idx..self.read_idx])?;
-            self.bytes_avail += written;
+            let write = buf.read(&mut self.buf[write_idx..self.read_idx])?;
+            self.bytes_avail += write;
+            written += write;
+            assert!(self.bytes_avail <= SIZE);
         }
 
         Ok(written)
@@ -74,12 +80,32 @@ impl<const SIZE: usize> RingBuffer<SIZE> {
         }
     }
 
+    pub fn buf(&self) -> &[u8] { &self.buf }
+
+    /// Copyless peek may reach end of buffer, the "wrap"
     pub fn peek(&self, len: usize) -> RBResult<&[u8]> {
         if len > SIZE - self.read_idx {
-            Err(RBErrorKind::NotEnoughBytesAvail)
+            Err(RBErrorKind::PeekReachedWrap)
         } else {
             Ok(&self.buf[self.read_idx..self.read_idx + len])
         }
+    }
+
+    /// 2 copies will occur if this peek reaches of the buffer "wrap".
+    /// One copy of the bytes present at the end of the buffer, and one
+    /// of the bytes present at the start of the buffer.
+    pub fn wrapping_peek(&mut self, len: usize) -> RBResult<&[u8]> {
+        if len > self.bytes_avail {
+            return Err(RBErrorKind::NotEnoughBytesAvail);
+        }
+
+        let end_bytes = cmp::min(SIZE - self.read_idx, len);
+        let start_bytes = self.bytes_avail - end_bytes;
+
+        self.peek_buffer[..end_bytes].copy_from_slice(&self.buf[self.read_idx..self.read_idx+end_bytes]);
+        self.peek_buffer[end_bytes..end_bytes+start_bytes].copy_from_slice(&self.buf[..start_bytes]);
+
+        Ok(&self.peek_buffer[..len])
     }
 }
 
@@ -132,5 +158,21 @@ mod tests {
         // We should have the same start of the internal buffer as previously
         let ans = [48u8, 48, 48, 55, 51, 13, 10, 58, 49, 48];
         assert_eq!(&rb.buf[..ans.len()], &ans);
+
+        // Currently we have consumed 10 bytes, so read_idx = 10, let's put it
+        // at 118 so we have 10 bytes in the rb right-of read_idx
+        rb.consume(108).expect("to be able to consume 108 bytes after 10 bytes");
+        assert_eq!(rb.read_idx, 118);
+
+        let last_10_bytes = &rb.buf[118..];
+        assert_eq!(last_10_bytes.len(), 10);
+        let first_10_bytes = &rb.buf[..10];
+        assert_eq!(first_10_bytes.len(), 10);
+        let mut linear_bytes = Vec::new();
+        linear_bytes.extend_from_slice(last_10_bytes);
+        linear_bytes.extend_from_slice(first_10_bytes);
+        // We need to be able to provide a peek wrapping the buffer
+        let peek = rb.wrapping_peek(linear_bytes.len()).expect("to be able to peek across buffer wrap");
+        assert_eq!(peek, &linear_bytes[..]);
     }
 }
